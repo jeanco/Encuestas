1b4a6378852b18ae57ad75feb629bbdcc6eb67c1
Rolando Vazquez Hernandez <rolando@rolando-Inspiron-N5110.(none)>
2012-09-05 11:27:30 -0600

    Se cambia direccion de redireccionamiento

--- app/controllers/encuestas_controller.rb
+++ app/controllers/encuestas_controller.rb
@@ -213,7 +213,7 @@ def capturar_datos
     @encuesta.save
     if session[:last_request] == contestar_web_url(@encuesta.id)
       session[:last_request] = nil
-      redirect_to "http://uach.mx/portal/2012/09/04/datos_guardados/"
+      redirect_to encuesta_gracias_web_url
     else
       redirect_to encuesta_gracias_url
     end
