6308a098af4a26c22d934005543239b3d07c357c
Rolando <rolando@rolando-VGN-NR160E.(none)>
2012-09-09 19:39:12 -0600

    Se elimina encuestado en caso de que no coincidan datos de autenticacion

--- app/controllers/usuario_sessions_controller.rb
+++ app/controllers/usuario_sessions_controller.rb
@@ -9,7 +9,7 @@ before_filter :authenticate, :only => :destroy
 
     u = Usuario.find_by_login(params[:usuario_session][:login])
     if u.nil?
-      @user = Usuario.create(:login => params[:usuario_session][:login], :email => params[:usuario_session][:login] + "@uach.mx", :password => "123pum")
+      @user = Usuario.create(:login => params[:usuario_session][:login], :email => params[:usuario_session][:login] + "@uach.mx", :password => "123pum", :password_confirmation => "123pum")
       @user.add_role "encuestado"
       @user.save
       @encuestado= Encuestado.create(:login => @user.login, :email => @user.email, :session_id => request.session_options[:id])
@@ -21,6 +21,7 @@ before_filter :authenticate, :only => :destroy
       redirect_back_or root_path
     else
       @user.delete if u.nil?
+      @encuestado.delete if u.nil?
       render :action => :new
     end
   end
